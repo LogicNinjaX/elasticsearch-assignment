68096ecf9c62532988047ca6208a67a068cda93b
Nitish <mail.logicninjax@gmail.com>
2025-07-13 19:12:28 +0530

    first commit

---
+++ src/test/java/com/example/elasticsearch_assignment/ElasticsearchAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearch_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
